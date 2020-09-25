bdfe8304cab174a1daf072087c48ec3adcd0a2e8
vladov200 <30537979+vladov200@users.noreply.github.com>
2020-09-25 14:24:27 -0400

    Update with question statement and source web link

--- src/main/java/numbers/MedianSortedArrays.java
+++ src/main/java/numbers/MedianSortedArrays.java
@@ -1,5 +1,12 @@
 package main.java.numbers;
 
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+ * return the median of the two sorted arrays.
+ * 
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/
+ *
+ */
 public class MedianSortedArrays {
 
 	/**
